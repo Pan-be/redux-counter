5ba9a95d28b944b9442f4c55df0c20a44ff6bf77
Konrad <konradbieniek@gmail.com>
2023-01-07 22:42:37 +0100

    toggling counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import { createStore } from "redux"
 
-const counterReducer = (state = { count: 0 }, action) => {
+const initialState = { count: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
 	if (action.type === "increment") {
 		return {
 			count: state.count + 1,
+			showCounter: state.showCounter,
+		}
+	}
+	if (action.type === "increase") {
+		return {
+			count: state.count + action.amount,
+			showCounter: state.showCounter,
 		}
 	}
 	if (action.type === "decrement") {
 		return {
 			count: state.count - 1,
+			showCounter: state.showCounter,
+		}
+	}
+	if (action.type === "toggle") {
+		return {
+			count: state.count,
+			showCounter: !state.showCounter,
 		}
 	}
 	return state
